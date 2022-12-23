925c9f50901220d9ab68cb96969c2a131cce7661
jcsestilo <jsestilo@up.edu.ph>
2022-12-23 21:11:03 +0800

    cmsc 170 progress

--- CMSC 170/estilojcs_minmax/estilojcs_minmax.py
+++ CMSC 170/estilojcs_minmax/estilojcs_minmax.py
@@ -10,6 +10,7 @@ from tkinter import messagebox
 PlayerUser = 'X'
 PlayerAI = 'O'
 stop_game = False
+NEG_INF = -99999
 
 player = messagebox.askquestion("Tic Tac Toe", "Should the user go first?\nYes - User (X)\nNo - AI (O)")
 
@@ -32,6 +33,24 @@ states = [
      [0,0,0],
      [0,0,0],
      [0,0,0]]
+
+s = [states] # start of the game
+p = ['P1', 'P2']
+
+def result(s,a): # -> s' : s’ are the new states made when action a is done to a state s.
+     print("result function")
+
+def actions(s,p): # provides the set of possible actions/moves for a player.
+     print("actions function")
+
+def terminal(): # It returns true when the state is terminal or there is an end result or false otherwise.
+     print("terminal function")
+
+def utility(s,p): # Returns the current value of the state s to a player p
+     # It is expresses the value of the current game state to
+     # the player. It is usually expressed as +/- numbers or
+     # 0's or 1's
+     print("utility function")
  
 for i in range(3):
     for j in range(3):
