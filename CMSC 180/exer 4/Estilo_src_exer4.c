99a4dde43213dd91ba1b1c0f2fe83d8692e65dc4
Jan Coleen Estilo <jsestilo@up.edu.ph>
2023-06-07 18:44:30 +0800

    update

--- CMSC 180/exer 4/Estilo_src_exer4.c
+++ CMSC 180/exer 4/Estilo_src_exer4.c
@@ -202,12 +202,19 @@ int main(){
             // serverAddress.sin_addr.s_addr = inet_addr(IPAddresses[i]);
             serverAddress.sin_addr.s_addr = INADDR_ANY; 
             
-            // Convert IP address from string to binary form
-            if (inet_pton(AF_INET, ipAddress, &(serverAddress.sin_addr)) <= 0) {
-                perror("Failed to convert IP address");
+            // Bind the socket to the server address
+            if (bind(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
+                perror("Failed to bind socket");
                 close(clientSocket);
                 return 1;
             }
+
+            // Convert IP address from string to binary form
+            // if (inet_pton(AF_INET, ipAddress, &(serverAddress.sin_addr)) <= 0) {
+            //     perror("Failed to convert IP address");
+            //     close(clientSocket);
+            //     return 1;
+            // }
 
             // Connect to the server
             if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
@@ -332,7 +339,7 @@ int main(){
         fclose(file);
 
         // Create client socket
-        serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+        serverSocket = socket(AF_INET, SOCK_STREAM, 1);
         if (serverSocket == -1) {
             perror("Failed to create client socket");
             return 1;
